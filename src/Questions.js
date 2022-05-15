0f9824f03f10fa76edf37caafe331a7221b6b5a0
CaioVitor1 <caiovitor33@hotmail.com>
2022-05-15 14:11:15 -0300

    marking answer

--- src/Questions.js
+++ src/Questions.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 
-export default function Questions({texto,resposta, index, estado, pergunta, mudaEstado, mudaPergunta}) {
+export default function Questions({mudaMarcadaErro, mudaMarcadaQuase,mudaMarcadaAcerto, marcada, texto,resposta, index, estado, pergunta, mudaEstado, mudaPergunta}) {
     
-    if(estado === "fechado" && texto === "false") {
+    if(estado === "fechado" && texto === "false" && marcada === "não") {
         return (
             <div  onClick={() => mudaEstado({index})} className='deck-question'>
                 <h3> Pergunta {index+1}</h3> 
@@ -11,7 +11,7 @@ export default function Questions({texto,resposta, index, estado, pergunta, muda
                 
             </div>
         )
-    } else if(estado == "aberto" && texto === "false"){
+    } else if(estado == "aberto" && texto === "false" && marcada === "não"){
         return (
             <div className='deck-question'>
                 <h3> {pergunta} </h3> 
@@ -19,20 +19,49 @@ export default function Questions({texto,resposta, index, estado, pergunta, muda
                 
             </div>
         )
-    } else if(estado == "aberto" && texto === "true"){
+    } else if(estado == "aberto" && texto === "true" && marcada === "não"){
         return (
             <div className='deck-respostas'>
                 <h3> {resposta} </h3> 
                 <div className="botoes">
-                    <button className="red"> Não lembrei!</button>
-                    <button className="blue"> Quase não lembrei!</button>
-                    <button className="green"> ZAP!</button>
+                    <button onClick={() => mudaMarcadaErro({index})} className="red"> Não lembrei!</button>
+                    <button onClick={() => mudaMarcadaQuase({index})} className="blue"> Quase não lembrei!</button>
+                    <button onClick={() => mudaMarcadaAcerto({index})} className="green"> ZAP!</button>
                 </div>
                 
             </div>
         )
+    } else if(estado == "aberto" && texto === "true" && marcada === "erro") {
+        return (
+            <div className='deck-marcacao'>
+              <h3 className="marcacao-red"> Pergunta {index+1}</h3> 
+                <img src='imagens/Vector_x.png'/>
+                
+            </div>
+        )
     }
+    else if(estado == "aberto" && texto === "true" && marcada === "quase") {
+        return (
+            <div className='deck-marcacao'>
+              <h3 className="marcacao-blue"> Pergunta {index+1}</h3> 
+                <img src='imagens/Vector_question.png'/>
+                
+            </div>
+        )
+    }
+    else if(estado == "aberto" && texto === "true" && marcada === "acerto") {
+        return (
+            <div className='deck-marcacao'>
+              <h3 className="marcacao-green"> Pergunta {index+1}</h3> 
+                <img src='imagens/Vector_ok.png'/>
+                
+            </div>
+        )
+    }
+    
    
+
+
 }
 
 
--- src/Screen2.js
+++ src/Screen2.js
@@ -5,22 +5,35 @@ import Footer from "./Footer";
 import { useState } from 'react/cjs/react.production.min';
 
 const everyQuestion = [
-    {id:1, pergunta: "O que é JSX?" , resposta: "Uma extensão de linguagem do JavaScript", estado: "fechado", texto:"false"},
-    {id:2, pergunta: "O React é __ " , resposta: "Uma biblioteca JavaScript para construção de interfaces", estado: "fechado", texto:"false"},
-    {id:3, pergunta: "Componentes devem iniciar com __" , resposta: "Letra maiúscula", estado: "fechado", texto:"false"},
-    {id:4, pergunta: "Podemos colocar __ dentro do JSX?" , resposta: "Expressões", estado: "fechado", texto:"false" },
-    {id:5, pergunta: "O ReactDOM nos ajuda __" , resposta: "Interagindo com a DOM para colocar componentes React na mesma", estado: "fechado", texto:"false"},
-    {id:6, pergunta: "Usamos o npm para __ " , resposta: "gerenciar os pacotes necessários e suas dependências", estado: "fechado", texto:"false"},
-    {id:7, pergunta: "Usamos props para __ " , resposta: "passar diferentes informações para componentes ", estado: "fechado", texto:"false"},
-    {id:8, pergunta: "Usamos estado (state) para __" , resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", estado: "fechado", texto:"false"}
+    {id:1, pergunta: "O que é JSX?" , resposta: "Uma extensão de linguagem do JavaScript", estado: "fechado", texto:"false", marcada: "não"},
+    {id:2, pergunta: "O React é __ " , resposta: "Uma biblioteca JavaScript para construção de interfaces", estado: "fechado", texto:"false", marcada: "não"},
+    {id:3, pergunta: "Componentes devem iniciar com __" , resposta: "Letra maiúscula", estado: "fechado", texto:"false", marcada: "não"},
+    {id:4, pergunta: "Podemos colocar __ dentro do JSX?" , resposta: "Expressões", estado: "fechado", texto:"false", marcada: "não" },
+    {id:5, pergunta: "O ReactDOM nos ajuda __" , resposta: "Interagindo com a DOM para colocar componentes React na mesma", estado: "fechado", texto:"false", marcada: "não"},
+    {id:6, pergunta: "Usamos o npm para __ " , resposta: "gerenciar os pacotes necessários e suas dependências", estado: "fechado", texto:"false", marcada: "não"},
+    {id:7, pergunta: "Usamos props para __ " , resposta: "passar diferentes informações para componentes ", estado: "fechado", texto:"false", marcada: "não"},
+    {id:8, pergunta: "Usamos estado (state) para __" , resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", estado: "fechado", texto:"false", marcada: "não"}
 ]
 //     {everyQuestion.map((card) => (<Questions pergunta={card.index}/>))}
 //      setTasks(tasks.filter((t, i) => i !== index));
 //                {tela === "screen1" ? <Screen1 setTela={setTela}/> : <Screen2 />}
 
+function Contador() {
+    const [contador, setContador] = React.useState(0);
+ 
+    
+    return(
+        <>
+         <h3> {contador}/8 concluídos </h3>  
+        </>
+    )
+   
+}
+
 function ExportQuestions() {
     const [state,setState] = React.useState("fechado");
     const [text, setText] = React.useState("false");
+    const [marked, setMarked] = React.useState("não")
 
     function mudaEstado({index}) {
     console.log(index)
@@ -42,18 +55,44 @@ function ExportQuestions() {
            } 
           
         }
+       function mudaMarcadaErro({index}) {
+        if(everyQuestion[index].marcada= "não") {
+            everyQuestion[index].marcada= "erro"
+            setMarked("erro")
+            console.log("terminamos de marcar")
+           } 
+          
+        }
+        function mudaMarcadaQuase({index}) {
+            if(everyQuestion[index].marcada= "não") {
+                everyQuestion[index].marcada= "quase"
+                setMarked("quase")
+                console.log("terminamos de marcar")
+               } 
+              
+            }
+            function mudaMarcadaAcerto({index}) {
+                if(everyQuestion[index].marcada= "não") {
+                    everyQuestion[index].marcada= "acerto"
+                    setMarked("erro")
+                    console.log("terminamos de marcar")
+                   } 
+                  
+                }
+
+       
 
 
 
     return ( 
     <div className='deck'>   
-            {everyQuestion.map((card,index) => (<Questions resposta={card.resposta} text={text} texto={card.texto} state={state} index={index} mudaPergunta={mudaPergunta} mudaEstado={mudaEstado}  titulo={card.titulo} estado={card.estado} pergunta={card.pergunta}/>))}
+            {everyQuestion.map((card,index) => (<Questions mudaMarcadaErro={mudaMarcadaErro} mudaMarcadaQuase={mudaMarcadaQuase} mudaMarcadaAcerto={mudaMarcadaAcerto} marked={marked} marcada={card.marcada} resposta={card.resposta} text={text} texto={card.texto} state={state} index={index} mudaPergunta={mudaPergunta} mudaEstado={mudaEstado}  titulo={card.titulo} estado={card.estado} pergunta={card.pergunta}/>))}
         </div>
         )
 }
 export default function Screen2() {
     
-    const [contador, setContador] = React.useState(0);
+   
     
    
     return (
@@ -63,7 +102,7 @@ export default function Screen2() {
 
             
             <Footer>
-                <h3> {contador}/8 concluídos </h3>  
+               <Contador />
             </Footer >
         </div>
     )
